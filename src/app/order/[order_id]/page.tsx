578bedfbc9af402c6d7865ee0f05d62a0057a85a
skiddythahypebeast <skiddyythahypebeast@gmail.com>
2024-12-06 12:01:15 +0800

    fix order page base url issue

--- src/app/order/[order_id]/page.tsx
+++ src/app/order/[order_id]/page.tsx
@@ -1,23 +1,32 @@
 import { type GetOrderResponse } from "../models";
 import { OrderDetails } from "../components/order_details";
 import { notFound } from "next/navigation";
+import { env } from "~/env";
+import { RequestType } from "~/app/swap/models";
 
 export default async function Order(props: { params: Promise<{ order_id: string }> }) {
   const params = await props.params;
-  const serverUrl = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000'; 
-  const data = await fetch(`${serverUrl}/api/order/${params.order_id}`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }
+  const order_id_buf = Buffer.from(params.order_id, 'hex');
+  const order_id = order_id_buf.toString('utf8');
+
+  if (!order_id) {
+    console.error(`Invalid order id: ${params.order_id}`);
+    notFound();
+  }
+
+  const response = await fetch(`${env.SERVER_URL}/${RequestType.GET_ORDER}?api_key=${env.API_KEY}&id=${order_id}`, {
+    method: 'GET',
+    headers: { "Content-Type": "application/json"},
   });
 
-  if (data.status == 404) {
+  if (response.status == 404) {
     notFound();
-  } else if (!data.ok) {
-    console.error(await data.json());
+  } else if (!response.ok) {
+    console.error(await response.json());
     throw new Error("Internal server error");
   }
 
-  const order_details = await data.json() as GetOrderResponse;
+  const order_details = await response.json() as GetOrderResponse;
   order_details.id = params.order_id;
 
   const currency_from = order_details.currencies[order_details.currency_from];
@@ -35,10 +44,10 @@ export default async function Order(props: { params: Promise<{ order_id: string 
   }
 
   return (
-      <OrderDetails 
-        order_id={params.order_id}
-        order_details={order_details} 
-        currency_from={currency_from} 
-        currency_to={currency_to} />
+    <OrderDetails 
+      order_id={params.order_id}
+      order_details={order_details} 
+      currency_from={currency_from} 
+      currency_to={currency_to} />
   );
 }
