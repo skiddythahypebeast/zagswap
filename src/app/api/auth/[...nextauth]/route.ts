dbdfb5ec914d2160573834669ac2740733882436
skiddythahypebeast <skiddyythahypebeast@gmail.com>
2024-12-07 14:43:36 +0800

    revert OPTIONS

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./auth-options";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextResponse, type NextRequest } from "next/server";
 
 export const GET = NextAuth(authOptions) as (request: NextRequest) => Promise<NextResponse>;
 export const POST = NextAuth(authOptions) as (request: NextRequest) => Promise<NextResponse>;
-export const OPTIONS = (_: NextRequest) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    };
-
-    return new NextResponse(null, { status: 200, headers });
-};
